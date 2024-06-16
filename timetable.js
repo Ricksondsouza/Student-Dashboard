7937fc0220aa06726e201b32d80ed6998fe3bf67
rickson dsouza <21d45.rickson@sjec.ac.in>
2024-06-16 18:59:37 +0530

    Create timetable.js

---
+++ timetable.js
@@ -0,0 +1,127 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'Engineering Mathematics',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'Signals And Systems',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:15-1:15 PM',
+        roomNumber: '38-718',
+        subject: 'DSP',
+        type: 'Lab'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-11 AM',
+        roomNumber: '27-304Y',
+        subject: 'Engineering Graphic Design',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11:15-1:15 PM',
+        roomNumber: '28-107',
+        subject: 'IOT',
+        type: 'Lab'
+    },
+    {   
+        time: '2-3 PM',
+        roomNumber: '28-107',
+        subject: 'Signals And Systems',
+        type: 'Lecture'
+    },
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'DSP',
+        type: 'Lab'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'Engineering Graphic Design',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'Engineering Mathematics',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'Engineering Graphic Design',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'Signals and Sysytems',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'IOT',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'DSP',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'Engineering Graphic Design',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'Engineering Mathematics',
+        type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'Signals And Systems',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'Engineering Mathematics',
+        type: 'Lecture'
+    }
+]
